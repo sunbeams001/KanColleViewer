0c7cf1b679b9b2877d0d8e33f03e9ebc2652377b
Clicia <yuyuvn@mac.com>
2014-07-31 19:04:33 +0700

    Fix UI Position

--- Grabacr07.KanColleViewer/Views/Converters/PreviewSizeConverter.cs
+++ Grabacr07.KanColleViewer/Views/Converters/PreviewSizeConverter.cs
@@ -13,10 +13,10 @@ namespace Grabacr07.KanColleViewer.Views.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (values.Length != 4) throw new ArgumentException();
+            if (values.Length != 6) throw new ArgumentException();
             String[] posString = ((String)parameter).Split(',');
-            Double posX = Double.Parse(posString[0]);
-            Double posY = Double.Parse(posString[1]);
+			Double posX = Double.Parse(posString[0]) + (Double)values[4];
+			Double posY = Double.Parse(posString[1]) + (Double)values[5];
             Double rootWidth = (Double)values[0];
             Double rootHeight = (Double)values[1];
             Double contentWidth = (Double)values[2];
