e04402eab34bc5b68fd7d9fbdc266d00273900ac
Clicia Scarlet <yuyuvn@mac.com>
2014-08-18 11:38:45 +0700

    add calculator plugins

---
+++ Plugins/Calculator/KancolleCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleViewer.Plugins.ViewModels;
+using Grabacr07.KanColleViewer.Plugins.Views;
+using Grabacr07.KanColleWrapper;
+
+namespace Grabacr07.KanColleViewer.Plugins
+{
+	[Export(typeof(IToolPlugin))]
+	[ExportMetadata("Title", "Calculator")]
+	[ExportMetadata("Description", "start2 で取得される、艦これのマスター データを閲覧するためのビュー機能を提供します。")]
+	[ExportMetadata("Version", "1.0")]
+	[ExportMetadata("Author", "@Grabacr07")]
+	public class KancolleCalculator : IToolPlugin
+	{
+        private CalculatorViewModel calculatorViewModel;
+
+		public string ToolName
+		{
+			get { return "Calculator"; }
+		}
+
+
+		public object GetSettingsView()
+		{
+			return null;
+		}
+
+		public object GetToolView()
+		{
+            if (this.calculatorViewModel == null)
+            {
+                if (KanColleClient.Current.Homeport != null) this.calculatorViewModel = new CalculatorViewModel();
+            }
+            return new Calculator { DataContext = this.calculatorViewModel };
+		}
+	}
+}
