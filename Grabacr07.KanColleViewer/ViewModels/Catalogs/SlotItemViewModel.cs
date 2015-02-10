f270a3dcbc9e55b48a4afe8a5fbb475b17b8275b
Clicia Scarlet <yuyuvn@mac.com>
2015-02-10 21:05:37 +0700

    Negative stats

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemViewModel.cs
@@ -20,11 +20,11 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			public Ship Ship { get; set; }
 			public int Count { get; set; }
 
-			public string ShipName =>  this.Ship.Info.Name;
+			public string ShipName => this.Ship.Info.Name;
 
-		    public string ShipLevel => "Lv." + this.Ship.Level;
+			public string ShipLevel => "Lv." + this.Ship.Level;
 
-		    public string CountString => this.Count == 1 ? "" : " x " + this.Count + " ";
+			public string CountString => this.Count == 1 ? "" : " x " + this.Count + " ";
 
 			public string StatsToolTip
 			{
@@ -35,7 +35,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 						AddDetail += this.Ship.Info.UntranslatedName + "\n";
 
 					foreach (SlotItem s in this.Ship.SlotItems) {
-						AddDetail += String.Format("{0}{1}\n",s.Info.Name,s.Level > 0 ? " +" + s.Level : "");
+						AddDetail += String.Format("{0}{1}\n", s.Info.Name, s.Level > 0 ? " +" + s.Level : "");
 					}
 
 					return AddDetail.TrimEnd('\n');
@@ -88,7 +88,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			get
 			{
 				string _Detail = this.Detail;
-				return this.SlotItem.Name + (this.Level > 0 ? " +"+this.Level : "") + (_Detail != "" ? "\n" + _Detail : "");
+				return this.SlotItem.Name + (this.Level > 0 ? " +" + this.Level : "") + (_Detail != "" ? "\n" + _Detail : "");
 			}
 		}
 
@@ -96,21 +96,21 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 		{
 			get
 			{
-				string AddDetail = "";
+				List<string> details = new List<string>();
 
-				if (this.SlotItem.Firepower> 0) AddDetail += " +" + this.SlotItem.Firepower + " " + Resources.Stats_Firepower;
-				if (this.SlotItem.AA > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.AA + " " + Resources.Stats_AntiAir;
-				if (this.SlotItem.Torpedo > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Torpedo + " " + Resources.Stats_Torpedo;
-				if (this.SlotItem.AntiSub > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.AntiSub + " " + Resources.Stats_AntiSub;
-				if (this.SlotItem.SightRange > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.SightRange + " " + Resources.Stats_SightRange;
-				if (this.SlotItem.Speed > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Speed + " " + Resources.Stats_Speed;
-				if (this.SlotItem.Armor > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Armor + " " + Resources.Stats_Armor;
-				if (this.SlotItem.Health > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Health + " " + Resources.Stats_Health;
-				if (this.SlotItem.Luck > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Luck + " " + Resources.Stats_Luck;
-				if (this.SlotItem.Evasion > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Evasion + " " + Resources.Stats_Evasion;
-				if (this.SlotItem.Accuracy > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.Accuracy + " " + Resources.Stats_Accuracy;
-				if (this.SlotItem.DiveBomb > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.DiveBomb + " " + Resources.Stats_DiveBomb;
-				if (this.SlotItem.AttackRange > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " " + Resources.Stats_AttackRange + " (" + this.SlotItem.AttackRange + ")";
+				if (this.SlotItem.Firepower != 0) details.Add(StatFormat(SlotItem.Firepower, Resources.Stats_Firepower));
+				if (this.SlotItem.AA != 0) details.Add(StatFormat(SlotItem.AA, Resources.Stats_AntiAir));
+				if (this.SlotItem.Torpedo != 0) details.Add(StatFormat(SlotItem.Torpedo, Resources.Stats_Torpedo));
+				if (this.SlotItem.AntiSub != 0) details.Add(StatFormat(SlotItem.AntiSub, Resources.Stats_AntiSub));
+				if (this.SlotItem.SightRange != 0) details.Add(StatFormat(SlotItem.SightRange, Resources.Stats_SightRange));
+				if (this.SlotItem.Speed != 0) details.Add(StatFormat(SlotItem.Speed, Resources.Stats_Speed));
+				if (this.SlotItem.Armor != 0) details.Add(StatFormat(SlotItem.Armor, Resources.Stats_Armor));
+				if (this.SlotItem.Health != 0) details.Add(StatFormat(SlotItem.Health, Resources.Stats_Health));
+				if (this.SlotItem.Luck != 0) details.Add(StatFormat(SlotItem.Luck, Resources.Stats_Luck));
+				if (this.SlotItem.Evasion != 0) details.Add(StatFormat(SlotItem.Evasion, Resources.Stats_Evasion));
+				if (this.SlotItem.Accuracy != 0) details.Add(StatFormat(SlotItem.Accuracy, Resources.Stats_Accuracy));
+				if (this.SlotItem.DiveBomb != 0) details.Add(StatFormat(SlotItem.DiveBomb, Resources.Stats_DiveBomb));
+				if (this.SlotItem.AttackRange > 0) details.Add(String.Format(" {1}({0})", this.SlotItem.AttackRange, Resources.Stats_AttackRange));
 				//if (this.SlotItem.RawData.api_raik > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.RawData.api_raik + " api_raik";
 				//if (this.SlotItem.RawData.api_raim > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.RawData.api_raim + " api_raim";
 				//if (this.SlotItem.RawData.api_sakb > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.RawData.api_sakb + " api_sakb";
@@ -118,8 +118,13 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 				//if (this.SlotItem.RawData.api_rare > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.RawData.api_rare + " api_rare";
 				//if (this.SlotItem.RawData.api_bakk > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.SlotItem.RawData.api_bakk + " api_bakk";
 
-				return AddDetail;
+				return String.Join("\n", details);
 			}
+		}
+
+		private string StatFormat(int stat, string name)
+		{
+			return String.Format(" {0:+#;-#} {1}", stat, name);
 		}
 	}
 }
