996fa5b257368d25dade39857bdcda35197ef9b1
Clicia Scarlet <yuyuvn@mac.com>
2015-03-01 00:44:24 +0700

    Add Lookout icon

--- Grabacr07.KanColleWrapper/Models/SlotItemIconType.cs
+++ Grabacr07.KanColleWrapper/Models/SlotItemIconType.cs
@@ -164,5 +164,10 @@ namespace Grabacr07.KanColleWrapper.Models
         /// ロケットランチャー。
         /// </summary>
         RocketLauncher = 31,
+
+        /// <summary>
+        /// 見張。
+        /// </summary>
+        Lookout = 32,
     }
 }
